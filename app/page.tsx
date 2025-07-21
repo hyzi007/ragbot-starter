6d8d67c007df8907d086e122affc3bbe566b338c
hyzi007 <37216403+hyzi007@users.noreply.github.com>
2025-07-21 10:56:31 +0200

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 import {useEffect, useRef, useState} from 'react';
 import Bubble from '../components/Bubble'
-import { useChat, Message } from 'ai/react';
+import { useChat, Message } from '@ai-sdk/react';
+
 import Footer from '../components/Footer';
 import Configure from '../components/Configure';
-import PromptSuggestionRow from '../components/PromptSuggestions/PromptSuggestionsRow';
+
 import ThemeButton from '../components/ThemeButton';
 import useConfiguration from './hooks/useConfiguration';
 
